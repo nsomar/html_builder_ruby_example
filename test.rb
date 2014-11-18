df45d56e40c6fa1f70c4bc67bfd0238e19d293b7
Omar Abdelhafith <omar.abdelhafith@mttnow.com>
2014-11-18 00:00:12 +0000

    renaming methods

--- test.rb
+++ test.rb
@@ -8,30 +8,30 @@ class HTMLBuilder
   end
 
   def add_tag(t) puts "##{t}" end
-  def open_head; puts "<html>" end
-  def open_body; puts "<body>" end
-  def close_head; puts "</body>" end
-  def close_body; puts "</html>" end
+  def add_head_open_tag; puts "<html>" end
+  def add_body_open_tag; puts "<body>" end
+  def add_body_close_tag; puts "</body>" end
+  def add_head_close_tag; puts "</html>" end
 end
 
 # DSL attempts
 
 # Attempt 1
 # HTMLBuilder.build do |h|
-#   h.open_head
-#   h.open_body
+#   h.add_head_open_tag
+#   h.add_body_open_tag
 #   h.add_tag("br")
-#   h.close_body
-#   h.close_head
+#   h.add_body_close_tag
+#   h.add_head_close_tag
 # end
 
 # Attempt 2
 # HTMLBuilder.build2 do
-#   open_head
-#   open_body
+#   add_head_open_tag
+#   add_body_open_tag
 #   add_tag("br")
-#   close_body
-#   close_head
+#   add_body_close_tag
+#   add_head_close_tag
 # end
 
 # Attempt 3
